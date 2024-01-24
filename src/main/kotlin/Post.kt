7460f663e6a8ab7a1e1dae7201455902b20f8c1e
Pavel Vinogradov <mr.friderick@ya.ru>
2024-01-24 17:12:33 +0300

    Добавлены nullable свойства для Post

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -3,9 +3,9 @@ data class Post(
     val ownerId: Int,
     val fromId: Int,
     val text: String,
-    val comments: Comments,
+    val comments: Comments?,
     val friendsOnly: Boolean,
-    val likes: Likes,
+    val likes: Likes?,
     val postType: String = "post",
     val canPin: Boolean = true,
     val canEdit: Boolean = true
